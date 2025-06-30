d12f42f4159af0533d7d166299f1f45f4638bd32
hibnu-putra <111202315463@mhs.dinus.ac.id>
2025-06-30 17:10:23 +0700

    menambahkan api data transaksi pembelian

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -30,11 +30,11 @@
         ?>
 
         <li class="nav-item">
-            <a class="nav-link <?php echo (uri_string() == 'profile') ? "" : "collapsed" ?>" href="profile">
-                <i class="bi bi-person"></i>
-                <span>Profile</span>
-            </a>
-        </li> <!-- End Profile Nav -->
+    <a class="nav-link <?php echo (uri_string() == 'profile') ? "" : "collapsed" ?>" href="profile">
+        <i class="bi bi-person"></i>
+        <span>Profile</span>
+    </a>
+</li><!-- End Profile Nav -->
 
         <li class="nav-item">
             <a class="nav-link <?php echo (uri_string() == 'faq') ? "" : "collapsed" ?>" href="faq">
